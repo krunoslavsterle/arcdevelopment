79ba655cc0562e0a21330e474fdd7652ee2fa22a
ksterle <krunoslavsterle@gmail.com>
2020-06-16 20:26:38 +0200

    - implementing routing

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/styles';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/styles";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { Link } from "react-router-dom";
 
-import logo from '../../assets/logo.svg';
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
   const { children } = props;
-  
+
   const trigger = useScrollTrigger({
     disableHysteresis: true,
-    threshold: 0
+    threshold: 0,
   });
 
   return React.cloneElement(children, {
@@ -23,60 +24,129 @@ function ElevationScroll(props) {
   });
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    
-    // This is hard-coded value that works in this case but we could rewrite the toolbar style inside the Theme so that the image changes the height with the toolbar. 
-    marginBottom: "3em"
+
+    // This is hard-coded value that works in this case but we could rewrite the toolbar style inside the Theme so that the image changes the height with the toolbar.
+    marginBottom: "3em",
   },
   logo: {
-    height: "7em"
+    height: "8em",
   },
   tabContainer: {
-    marginLeft: "auto"
+    marginLeft: "auto",
   },
   tab: {
     ...theme.typography.tab,
     minWidth: 10,
-    marginLeft: "25px"
+    marginLeft: "25px",
   },
   button: {
     ...theme.typography.estimate,
     borderRadius: "50px",
     marginLeft: "50px",
-    marginRight: "25px",    
+    marginRight: "25px",
     height: "45px",
-  }
-}))
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+}));
 
 export default function Header(props) {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
 
   return (
     <React.Fragment>
       <ElevationScroll>
         {/* position="fixed" and color="primary" are default values, we don't have to set them. */}
-          <AppBar position="fixed" color="primary">
+        <AppBar position="fixed" color="primary">
           {/* disableGutters removes the default padding around the Toolbar component */}
           <Toolbar disableGutters>
-            <img alt="company logo" src={logo} className={classes.logo} />
-            <Tabs className={classes.tabContainer}>
-              <Tab className={classes.tab} label="Home" />
-              <Tab className={classes.tab} label="Services" />
-              <Tab className={classes.tab} label="Revolution" />
-              <Tab className={classes.tab} label="About Us" />
-              <Tab className={classes.tab} label="Contact Us" />
+            <Button
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              disableRipple
+              onClick={() => setValue(0)}
+            >
+              <img alt="company logo" src={logo} className={classes.logo} />
+            </Button>
+            <Tabs
+              value={value}
+              className={classes.tabContainer}
+              onChange={handleChange}
+              indicatorColor="primary"
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="Revolution"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="about"
+                label="About Us"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="contact"
+                label="Contact Us"
+              />
             </Tabs>
-            <Button variant="contained" color="secondary" className={classes.button}>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
               Free Estimate
             </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
-      
+
       {/* toolbarMargin class contain height of the toolbar so this div will push the content bellow the toolbar (because the toolbar is not static, fixed flows above the content) */}
       <div className={classes.toolbarMargin} />
     </React.Fragment>
   );
-};
+}
