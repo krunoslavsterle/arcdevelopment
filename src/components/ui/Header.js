d53219c1b67583bfa1bf675bb4b75bed117a46f9
ksterle <krunoslavsterle@gmail.com>
2020-06-16 16:36:44 +0200

    - adding Tabs to Header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import { makeStyles } from '@material-ui/styles';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 import logo from '../../assets/logo.svg';
 
@@ -29,6 +31,14 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     height: "7em"
+  },
+  tabContainer: {
+    marginLeft: "auto"
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px"
   }
 }))
 
@@ -43,6 +53,13 @@ export default function Header(props) {
           {/* disableGutters removes the default padding around the Toolbar component */}
           <Toolbar disableGutters>
             <img alt="company logo" src={logo} className={classes.logo} />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
